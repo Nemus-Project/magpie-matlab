5a908df647fc604f22192760b0fd59c4fcfe0831
Matthew Hamilton <matthew.hamilton2@unibo.it>
2025-10-07 12:34:04 +0100

    0.0.2 documentation

--- docs/build_docs.m
+++ docs/build_docs.m
@@ -4,3 +4,5 @@ files = {d.name};  % cell array of filenames
 for i = 1:numel(files)    
     publish(files{i}, 'format', 'html','outputDir', './html','evalCode',false);
 end
+
+publish('index.m', 'format', 'html','outputDir', './html','evalCode',false);
---
+++ docs/index.m
@@ -0,0 +1,22 @@
+%% magpie
+% An open source MATLAB library for working with thin plates with generalized elastic boundary conditions. 
+% The framework is designed around two key use cases
+% 
+% # Exploration of plates for understanding how material properties affect mode shapes and modal frequencies.
+% # Derivation of material properties from experimental data.
+% 
+%%% Functions
+%
+% * <./magpie_help.html |magpie|>
+% * <./bhmat_help.html |bhmat|>
+% * <./youngcalc_help.html |youngcalc|>
+% * <./biharmdiag_help.html |biharmdiag|>
+% * <./fidimat_help.html |fidimat|>
+% * <./bhcoefs_help.html |bhcoefs|>
+% 
+%%% References
+% 
+% * Howard, & Angus, J. A. S. (2009). _Acoustics and psychoacoustics_ (4th ed..).
+% * Ashby, (October 2021) [_Material property data for engineering materials_](https://www.ansys.com/content/dam/amp/2021/august/webpage-requests/education-resources-dam-upload-batch-2/material-property-data-for-eng-materials-BOKENGEN21.pdf) Department of Engineering, University of Cambridge (5th edition)
+%  
+%% <https://github.com/Nemus-Project/magpie-matlab Fork magpie on GitHub>
